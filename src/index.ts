a7adef66e16ca38a06b3a550eb27b222abcf796e
DsGrilo <danielsgrilo@hotmail.com>
2021-10-16 21:40:44 -0300

    import express and configure server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+import express, { Request, Response, NextFunction } from 'express';
 
+// gerenciador de rotas que permite fazer req e res via http
+const app = express ();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+        res.status(200).send({ foo: 'bar'});
+});
+
+
+app.listen(3000, () => { 
+    console.log("Application executing in port 3000")
+})
 
 console.log("Executed")
