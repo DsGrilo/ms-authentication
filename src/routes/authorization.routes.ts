f5596650c02e3c5bb84c60db72f186e35d518cb1
DsGrilo <danielsgrilo@hotmail.com>
2021-10-17 13:09:45 -0300

    Adicionado expiração de token

--- src/routes/authorization.routes.ts
+++ src/routes/authorization.routes.ts
@@ -1,11 +1,10 @@
 import { Router, Request, Response, NextFunction } from "express";
-import errorHandler from "../middlewares/error.handler.middleware";
 import ForbiddenError from "../models/errors/forbiddden.error.models";
-import userRepository from "../repositories/user.repository";
-import  JWT from 'jsonwebtoken';
+import  JWT, { SignOptions } from 'jsonwebtoken';
 import { StatusCodes } from "http-status-codes";
 import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
 import jwtAuthenticationMiddleware from "../middlewares/jwt-authentication.middleware";
+
 
 const authorizationRoute = Router();
 
@@ -23,7 +22,7 @@ authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Req
 
 
         const jwtPayload = { username: user!.username};
-        const jwtOptions = { subject: user?.uuid };
+        const jwtOptions:SignOptions = { subject: user?.uuid, expiresIn: '1m'};
         const secretKey = 'my_secret_key'
 
         const jwt = JWT.sign(jwtPayload , secretKey , jwtOptions);
